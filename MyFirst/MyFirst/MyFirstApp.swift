6a1c75387c82b5b53281e221c327ed08456843e4
nananaNN <naoka0127@gmail.com>
2022-11-16 18:39:23 +0900

    MyFirstの追加

---
+++ MyFirst/MyFirst/MyFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstApp.swift
+//  MyFirst
+//
+//  Created by naoka.nakagawa on 2022/11/08.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
