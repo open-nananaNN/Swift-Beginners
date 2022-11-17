0e66411a856470c08c02f50491fa4bb65e6388e1
nananaNN <naoka0127@gmail.com>
2022-11-17 17:57:08 +0900

    Mymapの追加

--- MyMap/MyMap/ContentView.swift
+++ MyMap/MyMap/ContentView.swift
@@ -8,11 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    //入力中の文字列を保持する状態変数
+    @State var inputText: String = ""
+    
+    //検索キーワードを保持する状態変数
+    @State var displaySearchKey: String = ""
+    
+    //マップ種類　最初は標準から
+    @State var displayMapType: MapType = .standard
+    
     var body: some View {
         //垂直にレイアウト
         VStack {
-            //マップ表示
-            MapView(searchKey: "羽田空港")
+            
+            //テキストフィールド（文字入力）
+            TextField("キーワード", text: $inputText, prompt: Text("キーワードを入力してください"))
+            //入力が完了された時
+                .onSubmit{
+                    //入力が完了したので検索キーワードに設定する
+                    displaySearchKey = inputText
+                }
+            
+            //余白追加
+                .padding()
+            
+            //奥から手前方向にレイアウト（右下基準で配置）
+            ZStack(alignment: .bottomTrailing){
+                
+                //マップ表示
+                MapView(searchKey: displaySearchKey, mapType: displayMapType)
+                
+                //マップ種類切り替えボタン
+                Button {
+                    //標準→衛星→衛星＋交通機関ラベル
+                    if displayMapType == .standard{
+                        displayMapType = .satelite
+                    } else if displayMapType == .satelite{
+                        displayMapType = .hybrid
+                    } else {
+                        displayMapType = .standard
+                    }
+                    
+                } label:{
+                    //マップアイコンの表示
+                    Image(systemName: "map")
+                        .resizable()
+                        .frame(width: 35.0, height: 35.0)
+                }  //Buttonここまで
+                
+                //右下の余白を20空ける
+                .padding(.trailing, 20.0)
+                //下の余白を30あける
+                .padding(.bottom, 30.0)
+                
+            }//ZStackここまで
+            
         }//VStackここまで
 
     }//Viewここまで
--- MyMap/MyMap/MapView.swift
+++ MyMap/MyMap/MapView.swift
@@ -8,9 +8,18 @@
 import SwiftUI
 import MapKit
 
+//マップの種類を示す列挙型
+enum MapType {
+    case standard //標準
+    case satelite //衛星写真
+    case hybrid //衛星写真＋交通期間ラベル
+}
+
 struct MapView: UIViewRepresentable {
     //検索キーワード
     let searchKey: String
+    //マップ種類
+    let mapType: MapType
     
     //表示するViewを作成する時に実行
     func makeUIView(context: Context) -> MKMapView{
@@ -23,12 +32,65 @@ struct MapView: UIViewRepresentable {
         
         //入力された文字をデバッグエリアに表示
         print("検索キーワード:\(searchKey)")
+        
+        //マップの種類の設定
+        switch mapType {
+        case .standard:
+            //標準
+            uiView.preferredConfiguration = MKStandardMapConfiguration(elevationStyle: .flat)
+        case .satelite:
+            //衛星
+            uiView.preferredConfiguration = MKImageryMapConfiguration()
+        case .hybrid:
+            //衛星＋交通機関ラベル
+            uiView.preferredConfiguration = MKHybridMapConfiguration()
+        }
+        
+        //CLGeocoderインスタンス生成
+        let grecoder = CLGeocoder()
+        
+        //入力された文字から位置情報を取得
+        grecoder.geocodeAddressString(
+            searchKey,
+            completionHandler:{ (placemarks, error) in
+                //リクエストの結果が存在し1件目の情報から位置情報を取り出す
+                if let placemarks,
+                   let firstPlacemark = placemarks.first,
+                   let location = firstPlacemark.location{
+                    
+                    //位置情報から緯度経度をtargetCoodinateに取り出す
+                    let targetCoordinate = location.coordinate
+                    
+                    //経度緯度をでバックエリアに表示
+                    print("緯度経度：\(targetCoordinate)")
+                    
+                    //MKPointAnnotationインスタンスを作成しピンを作る
+                    let pin = MKPointAnnotation()
+                    
+                    //ピンを置く場所に緯度経度を設定
+                    pin.coordinate = targetCoordinate
+                    
+                    //ピンのタイトルを設定
+                    pin.title = searchKey
+                    
+                    //ピンを地図上に置く
+                    uiView.addAnnotation(pin)
+                    
+                    //緯度経度を中心に半径500mの範囲を表示
+                    uiView.region = MKCoordinateRegion(
+                        center: targetCoordinate,
+                    latitudinalMeters: 500.0,
+                    longitudinalMeters: 500.0)
+                    
+                }//ifここまで
+            } )//geocoderここまで
+        
     }//updateUIViewここまで
     
 }//MapView構造体の終了
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
-        MapView(searchKey: "羽田空港")
+        MapView(searchKey: "羽田空港", mapType: .standard)
     }
 }//PreViewここまで
